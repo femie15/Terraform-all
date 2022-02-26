507e14d03e13e867c7f5e98a3a6e1cc68d89a684
femie15 <33351638+femie15@users.noreply.github.com>
2022-02-26 14:31:49 +0100

    init

---
+++ GCP/main.tf
@@ -0,0 +1,57 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.73.0"
+    }
+  }
+
+  required_version = ">= 0.15.0"
+}
+
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+
+module "project_services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  version = "3.3.0"
+
+  project_id = "casha-339313"
+
+  activate_apis = [
+    "compute.googleapis.com",
+    "oslogin.googleapis.com"
+  ]
+
+  disable_services_on_destroy = false
+  disable_dependent_services  = false
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+
+  allow_stopping_for_update = true
+}
